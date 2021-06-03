14eb66d8cbe78b5ec6f160115ae82267f94178a2
massimez <massi.mez@ya.ru>
2021-06-04 02:43:51 +0700

    ✅ Add pagination nav

--- src/components/NavPagination.js
+++ src/components/NavPagination.js
@@ -1,25 +1,18 @@
 import React, { useState } from "react";
 
-export const NavPagination = ({ postsPerPage, totalPosts, paginate }) => {
+export const NavPagination = ({ charactersPerPage, totalCharacters, setCurrentPage,currentPage }) => {
   const pageNumbers = [];
   let i = 1;
-  for (; i <= totalPosts / postsPerPage + 1; i++) {
+  for (; i <= totalCharacters / charactersPerPage + 1; i++) {
     pageNumbers.push(i);
   }
 
   return (
       <div className="flex flex--center">
-         <div class="pagination center">
-        <a href="#">&laquo;</a>
-        <a href="#">1</a>
-        <a href="#" class="active">
-          2
-        </a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">6</a>
-        <a href="#">&raquo;</a>
+         <div className="pagination center">
+        {pageNumbers.map(n => <button className={currentPage === n ? "active":null } key={n}  onClick={() => {
+                           setCurrentPage(n)
+                           }} >{n}</button>)}
       </div> 
       </div>
       
